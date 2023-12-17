f7d14271adfed7adb9f25269380fe62fe087395e
slee1996 <spencerlee96@gmail.com>
2023-12-16 19:24:13 -0700

    fix: fix

--- components/ui/nav.dropdown-menu.tsx
+++ components/ui/nav.dropdown-menu.tsx
@@ -1,60 +1,60 @@
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "./dropdown-menu";
-import {
-  DropdownItem,
-  DropdownLink,
-  TOP_NAV_DROPDOWN_ITEMS,
-} from "@/constants/top-nav-dropdown-items";
-import Link from "next/link";
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuLabel,
+//   DropdownMenuSeparator,
+//   DropdownMenuTrigger,
+// } from "./dropdown-menu";
+// import {
+//   DropdownItem,
+//   DropdownLink,
+//   TOP_NAV_DROPDOWN_ITEMS,
+// } from "@/constants/top-nav-dropdown-items";
+// import Link from "next/link";
 
-// <DropdownMenu key={i}>
-//   <DropdownMenuTrigger className="capitalize">
-//     {item.name}
-//   </DropdownMenuTrigger>
-//   <DropdownMenuContent>
-//     <DropdownMenuItem>
-//       <Link href={`/${item.name}`} className="h-full w-full capitalize">
-//         All {item.name}
-//       </Link>
-//     </DropdownMenuItem>
-//     <DropdownMenuSeparator />
-//     {item.links.map((link: DropdownLink, i: any) => (
-//       <DropdownMenuItem key={i}>
-//         <Link href={link.href} className="h-full w-full capitalize">
-//           {link.name}
-//         </Link>
-//       </DropdownMenuItem>
-//     ))}
-//   </DropdownMenuContent>
-// </DropdownMenu>
+// // <DropdownMenu key={i}>
+// //   <DropdownMenuTrigger className="capitalize">
+// //     {item.name}
+// //   </DropdownMenuTrigger>
+// //   <DropdownMenuContent>
+// //     <DropdownMenuItem>
+// //       <Link href={`/${item.name}`} className="h-full w-full capitalize">
+// //         All {item.name}
+// //       </Link>
+// //     </DropdownMenuItem>
+// //     <DropdownMenuSeparator />
+// //     {item.links.map((link: DropdownLink, i: any) => (
+// //       <DropdownMenuItem key={i}>
+// //         <Link href={link.href} className="h-full w-full capitalize">
+// //           {link.name}
+// //         </Link>
+// //       </DropdownMenuItem>
+// //     ))}
+// //   </DropdownMenuContent>
+// // </DropdownMenu>
 
-export const NavDropDown = () => {
-  return TOP_NAV_DROPDOWN_ITEMS.map((item: DropdownItem, i: any) => (
-    <DropdownMenu key={i}>
-      <DropdownMenuTrigger className="capitalize">
-        {item.name}
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuItem>
-          <Link href={`/${item.name}`} className="h-full w-full capitalize">
-            All {item.name}
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        {item.links.map((link: DropdownLink, i: any) => (
-          <DropdownMenuItem key={i}>
-            <Link href={link.href} className="h-full w-full capitalize">
-              {link.name}
-            </Link>
-          </DropdownMenuItem>
-        ))}
-      </DropdownMenuContent>
-    </DropdownMenu>
-  ));
-};
+// export const NavDropDown = () => {
+//   return TOP_NAV_DROPDOWN_ITEMS.map((item: DropdownItem, i: any) => (
+//     <DropdownMenu key={i}>
+//       <DropdownMenuTrigger className="capitalize">
+//         {item.name}
+//       </DropdownMenuTrigger>
+//       <DropdownMenuContent>
+//         <DropdownMenuItem>
+//           <Link href={`/${item.name}`} className="h-full w-full capitalize">
+//             All {item.name}
+//           </Link>
+//         </DropdownMenuItem>
+//         <DropdownMenuSeparator />
+//         {item.links.map((link: DropdownLink, i: any) => (
+//           <DropdownMenuItem key={i}>
+//             <Link href={link.href} className="h-full w-full capitalize">
+//               {link.name}
+//             </Link>
+//           </DropdownMenuItem>
+//         ))}
+//       </DropdownMenuContent>
+//     </DropdownMenu>
+//   ));
+// };
