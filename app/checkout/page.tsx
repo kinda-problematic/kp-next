daf0dbd45436fee982e1e3b8fdbd6b161cddb573
slee1996 <spencerlee96@gmail.com>
2024-05-21 22:54:00 -0600

    demo mode

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,43 +1,7 @@
-"use client";
-import { useShoppingCart } from "@/context/shopping-cart";
-import { useState, useEffect, Suspense } from "react";
-import { loadStripe } from "@stripe/stripe-js";
-import {
-  EmbeddedCheckoutProvider,
-  EmbeddedCheckout,
-} from "@stripe/react-stripe-js";
-
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
-);
-
 export default function Checkout() {
-  const [clientSecret, setClientSecret] = useState("");
-  const cartContext = useShoppingCart();
-
-  useEffect(() => {
-    async function getCartSession() {
-      const res = await fetch("/api/checkout", {
-        method: "POST",
-        body: JSON.stringify(cartContext.shoppingCart),
-      });
-      const awaited = await res.json();
-      setClientSecret(awaited.clientSecret);
-    }
-
-    if (cartContext.shoppingCart.length > 0) {
-      getCartSession();
-    }
-  }, [cartContext.shoppingCart]);
-
-  return clientSecret ? (
+  return (
     <div className="flex justify-center">
-      <EmbeddedCheckoutProvider
-        stripe={stripePromise}
-        options={{ clientSecret }}
-      >
-        <EmbeddedCheckout className="p-4 bg-white rounded-lg w-4/5" />
-      </EmbeddedCheckoutProvider>
+      This site just a demo now! Thanks for checking it out.
     </div>
-  ) : null;
+  );
 }
